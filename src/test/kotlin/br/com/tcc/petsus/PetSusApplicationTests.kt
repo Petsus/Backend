8588191abc0c2b3df2291444dc4baa3d50fb43d3
Yuki Makoto <gluanfernandes13@gmail.com>
2023-05-04 23:45:06 -0300

    Removendo testes

--- src/test/kotlin/br/com/tcc/petsus/PetSusApplicationTests.kt
+++ src/test/kotlin/br/com/tcc/petsus/PetSusApplicationTests.kt
@@ -1,14 +1,14 @@
 package br.com.tcc.petsus
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class PetSusApplicationTests {
-
-    @Test
-    fun contextLoads() {
-        assert(true)
-    }
-
-}
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class PetSusApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//        assert(true)
+//    }
+//
+//}
--- src/test/kotlin/br/com/tcc/petsus/StateDto.kt
+++ src/test/kotlin/br/com/tcc/petsus/StateDto.kt
@@ -3,7 +3,7 @@ package br.com.tcc.petsus
 import java.io.Serializable
 import java.util.*
 
-/**
- * A DTO for the {@link br.com.tcc.petsus.model.address.state.State} entity
- */
-data class StateDto(val id: Long? = null, val createdAt: Date? = null, val updatedAt: Date? = null, val name: String? = null, val initials: String? = null, val ibgeId: Int? = null) : Serializable
+///**
+// * A DTO for the {@link br.com.tcc.petsus.model.address.state.State} entity
+// */
+//data class StateDto(val id: Long? = null, val createdAt: Date? = null, val updatedAt: Date? = null, val name: String? = null, val initials: String? = null, val ibgeId: Int? = null) : Serializable
