a131d9c41869ba92eb7c58be6f9f93b676b03051
Yuki Makoto <gluanfernandes13@gmail.com>
2023-11-22 04:06:44 -0300

    Default

--- src/main/kotlin/br/com/tcc/petsus/infrastructure/cors/CorsConfiguration.kt
+++ src/main/kotlin/br/com/tcc/petsus/infrastructure/cors/CorsConfiguration.kt
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@Configuration
+/*@Configuration
 class CorsConfiguration: WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
+            .allowedOrigins("http://petsus.app.br/")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
+ */
