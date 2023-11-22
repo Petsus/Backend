24cd95a07fb501da4419e115cac12c2bdd041c82
Yuki Makoto <gluanfernandes13@gmail.com>
2023-11-22 03:27:05 -0300

    Ajuste do cors

--- src/main/kotlin/br/com/tcc/petsus/infrastructure/security/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/tcc/petsus/infrastructure/security/SecurityConfiguration.kt
@@ -16,9 +16,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebSecurity
@@ -76,15 +73,4 @@ class SecurityConfiguration (
     override fun authenticationManager(): AuthenticationManager {
         return super.authenticationManager()
     }
-
-    @Bean
-    fun corsConfiguration(): WebMvcConfigurer {
-        return object : WebMvcConfigurerAdapter() {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**")
-                    .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
-            }
-        }
-    }
-
 }
